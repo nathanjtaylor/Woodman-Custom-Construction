f0cce048d08c8b79a8dad660ffaa26bb1ba47656
Nathan <LilFuze9@gmail.com>
2024-10-27 11:34:58 -0400

    load_content changes

--- js/load_content.js
+++ js/load_content.js
@@ -3,7 +3,17 @@
 
 // Grab the category from the URL and update the title
 const params = new URLSearchParams(window.location.search);
-const category = params.get('category') || 'Projects'; // Default if no category is selected
+const category = params.get('category') || ''; // Default if no category is selected
+const contentFolder = `content/project-${category}`; // Get content project folder
 
 // Update the header title with the category
 document.getElementById('category-title').innerText = `${category} Projects`;
+
+async function fetchProjects() {
+    const response = await fetch(`/${contentFolder}`);
+    const projectFiles = await response.json();
+    console.log(projectFiles);
+}
+
+document.addEventListener("DOMContentLoaded", fetchProjects);
+

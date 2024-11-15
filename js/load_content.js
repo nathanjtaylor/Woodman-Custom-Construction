f1e71fad45cea06ac1855643d8d03317820acec8
Nathan <LilFuze9@gmail.com>
2024-11-15 17:45:21 -0500

    single line gallery fix 2

--- js/load_content.js
+++ js/load_content.js
@@ -45,7 +45,7 @@ async function parseResponse(response){
           isInGallery = true; // Begin processing gallery items
         } else{
           // Single line gallery
-          projectData[galleryLine[0].trim()] = galleryLine[1].trim();
+          projectData[galleryLine[0].trim()] = galleryLine.slice(1).join(':').trim();
         }
       } else if (isInGallery && line.startsWith("  -")) {
         // If in the gallery, push image URLs into the array
@@ -72,6 +72,7 @@ async function parseResponse(response){
     return null;
   }
 }
+
 
 
 // Function to create an array of json files in catagory-specific content folder
